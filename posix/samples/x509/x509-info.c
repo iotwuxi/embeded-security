1f7bf0d0e8105fc30aebf2e83c0467a219425e0a
xukai <xukai19871105@126.com>
2017-05-26 11:06:39 +0800

    删除多余头文件

--- posix/samples/x509/x509-info.c
+++ posix/samples/x509/x509-info.c
@@ -18,33 +18,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "mbedtls/entropy.h"
-#include "mbedtls/ctr_drbg.h"
 #include "mbedtls/certs.h"
 #include "mbedtls/x509.h"
 #include "mbedtls/ssl.h"
-#include "mbedtls/ssl_cookie.h"
-#include "mbedtls/net_sockets.h"
-#include "mbedtls/error.h"
-#include "mbedtls/debug.h"
-#include "mbedtls/timing.h"
-
-#if defined(MBEDTLS_SSL_CACHE_C)
-#include "mbedtls/ssl_cache.h"
-#endif
-
-#define READ_TIMEOUT_MS 10000   /* 5 seconds */
-#define DEBUG_LEVEL 0
-
-static void my_debug( void *ctx, int level,
-                      const char *file, int line,
-                      const char *str )
-{
-    ((void) level);
-
-    mbedtls_fprintf( (FILE *) ctx, "%s:%04d: %s", file, line, str );
-    fflush(  (FILE *) ctx  );
-}
 
 int main( void )
 {
