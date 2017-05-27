0ed641e6204656f24d537f291efecbb4c1dfe0dc
xianrenqiu90@qq.com <xianrenqiu90@qq.com>
2017-05-27 17:24:19 +0800

    删除md5算法示例

--- posix/samples/hash/md5.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-const hash = crypto.createHash('md5');
-
-hash.update('Hello, world!');
-console.log(hash.digest('hex'));
--- posix/samples/hash/sha256.c
+++ posix/samples/hash/sha256.c
@@ -1,43 +1,22 @@
-#if !defined(MBEDTLS_CONFIG_FILE)
-#include "mbedtls/config.h"
-#else
-#include MBEDTLS_CONFIG_FILE
-#endif
-
-#if defined(MBEDTLS_PLATFORM_C)
-#include "mbedtls/platform.h"
-#else
 #include <stdio.h>
-#define mbedtls_printf     printf
-#endif
-
-#if defined(MBEDTLS_SHA256_C)
 #include "mbedtls/sha256.h"
-#endif
 
-#if !defined(MBEDTLS_SHA256_C)
-int main( void )
-{
-    mbedtls_printf("MBEDTLS_SHA256_C not defined.\n");
-    return( 0 );
-}
-#else
 int main( void )
 {
     int i;
     unsigned char digest[32];
     char str[] = "Hello, world!";
 
-    mbedtls_printf( "\n  SHA256('%s') = ", str );
+    printf( "\n  SHA256('%s') = ", str );
 
     mbedtls_sha256( (unsigned char *) str, 13, digest, 0 );
 
     for( i = 0; i < 32; i++ )
-        mbedtls_printf( "%02x", digest[i] );
-
-    mbedtls_printf( "\n\n" );   
+    {
+        printf( "%02x", digest[i] );
+    }
+    printf( "\n\n" );   
 
     return( 0 );
 }
-#endif /* MBEDTLS_SHA256_C */
 
