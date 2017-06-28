562683f681fa1800b350e47036936d39b715a554
cuihongpeng <cuihongpeng@tsinghua-wx.org>
2017-06-28 12:52:28 +0800

    修改目录结构，将posix示例移动到tests 目录下

--- posix/samples/hash/sha256.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-const hash = crypto.createHash('sha256');
-
-hash.update('Hello, world!');
-console.log(hash.digest('hex'));
--- posix/samples/hmac/hmac-demo.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-const hmac = crypto.createHmac('sha256', 'a secret');
-
-hmac.update('some data to hash');
-console.log(hmac.digest('hex'));
-// Prints:
-// 7fd04df92f636fd450bc841c9418e5825c17f33ad9c87c518115a45971f7f77e
