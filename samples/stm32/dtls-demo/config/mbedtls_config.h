a30e8f26899d9563e67bc55150e9f2450ada0dc9
cuihongpeng <cuihongpeng@tsinghua-wx.org>
2017-07-31 13:37:02 +0800

    修改配置文件说明

--- samples/stm32/dtls-demo/config/mbedtls_config.h
+++ samples/stm32/dtls-demo/config/mbedtls_config.h
@@ -1,23 +1,23 @@
 #ifndef STM32F7_MBEDTLS_CONFIG_H
 #define STM32F7_MBEDTLS_CONFIG_H
 
-/** 平台相关 */
+/** 平台相关 +++ >>> stm32 开启, Linux 关闭 */
 #if 1                                         
-#define MBEDTLS_TIMING_ALT                      /** stm32 开启, Linux 关闭 */
-#define MBEDTLS_ENTROPY_HARDWARE_ALT            /** stm32 开启, Linux 关闭 */
-#define MBEDTLS_NO_PLATFORM_ENTROPY             /** stm32 开启, Linux 关闭 */
+#define MBEDTLS_TIMING_ALT                      /** 时间接口替换 */
+#define MBEDTLS_ENTROPY_HARDWARE_ALT            /** 熵源接口替换 */
+#define MBEDTLS_NO_PLATFORM_ENTROPY             /** 无平台熵源支持 (linux: /dev/urandom 或 Windows CryptoAPI) */
 #endif
 
-/** Debug */
+/** 调试 定义 */
 #define MBEDTLS_DEBUG_C                         /** 调试(需要修改 DEBUG_LEVEL 值，默认为0) */
 
-/** System support */
+/** 系统 定义 */
 #define MBEDTLS_NET_C                           /** 网络接口(net_sockets.c) */
 #define MBEDTLS_HAVE_ASM                        /** 开启 asm 支持 */
 #define MBEDTLS_HAVE_TIME                       /** 开启 time() 支持 */
 #define MBEDTLS_TIMING_C                        /** 开启 时间延时接口(for dtls) 支持 */
 
-/** mbedtls feature support */
+/** SSL 定义 */
 #define MBEDTLS_SSL_TLS_C                       /** 开启 TLS, 生成 SSL 相关代码(ssl_tls.c ssl_cli.c ssl_srv.c) */
 #define MBEDTLS_SSL_CLI_C                       /** 开启 SSL Client(ssl_cli.c) */
 #define MBEDTLS_SSL_SRV_C                       /** 开启 SSL Server(ssl_srv.c) */
@@ -27,50 +27,56 @@
 #define MBEDTLS_SSL_PROTO_TLS1_2                /** 选择 TLS 协议版本为1.2 */
 
 
-/** mbedtls bignum modules */
+/** 数据解析 定义*/
 #define MBEDTLS_OID_C                           /** X.509 RSA等功能依赖于此定义 */
 #define MBEDTLS_ASN1_PARSE_C                    /** X.509 相关功能依赖于此定义 */
 #define MBEDTLS_ASN1_WRITE_C                    /** X.509 相关功能依赖于此定义 */
 #define MBEDTLS_BIGNUM_C                        /** X.509 ECC RSA等功能依赖于此定义 */
 
-/** mbedtls cipher modules */
+/** (熵)随机数 定义 */
+#define MBEDTLS_ENTROPY_C                       /** 熵 */
+#define MBEDTLS_CTR_DRBG_C                      /** 随机数发生器 */
+
+/** 对称密码 定义 */
 #define MBEDTLS_AES_C                           /** AES 加密 */
 #define MBEDTLS_CCM_C                           /** CCM 模式 */
 #define MBEDTLS_GCM_C                           /** GCM 模式 */
 #define MBEDTLS_CIPHER_C                        /** 开启 加密模块接口 */
-#define MBEDTLS_ENTROPY_C                       /** 熵 */
-#define MBEDTLS_CTR_DRBG_C                      /** 随机数发生器 */
+
+/** 消息摘要 定义 */
 #define MBEDTLS_SHA1_C                          /** SHA1 */
 #define MBEDTLS_SHA256_C                        /** SHA256 */
 #define MBEDTLS_SHA512_C                        /** SHA512 */
 #define MBEDTLS_MD_C                            /** 开启 消息摘要接口 */
 
-/** mbedtls public key modules */
+/** 公钥密码 定义 */
 #define MBEDTLS_PK_C                            /** 开启 公钥密码接口 */
 #define MBEDTLS_PK_PARSE_C                      /** 公钥密码解析 */
 
-/** mbedtls RSA modules */
+/** RSA 定义 */
 #define MBEDTLS_RSA_C                           /** RSA 算法 */
 #define MBEDTLS_PKCS1_V15                       /** 支持 PKCS1_V15 公钥标准 */
 #define MBEDTLS_PKCS1_V21                       /** 支持 PKCS1_V21 公钥标准 */
 
-/** mbedtls ECC modules */
+/** ECC 定义 */
 #define MBEDTLS_ECP_C                               /** ECC 算法 */
 #define MBEDTLS_ECDH_C                              /** ECDH 算法 */
 #define MBEDTLS_ECDSA_C                             /** ECDSA 算法 */
 #define MBEDTLS_ECP_DP_SECP384R1_ENABLED            /** 特殊 ECC 参数 */
-#define MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED    /** 开启 ECDHE_ECDSA 密钥协商 */
-#define MBEDTLS_KEY_EXCHANGE_ECDHE_RSA_ENABLED      /** 开启 ECDHE_RSA 密钥协商 */
-#define MBEDTLS_KEY_EXCHANGE_PSK_ENABLED            /** 开启 PSK 密钥协商 */
 
-/** mbedtls certificates */
+/** 证书 定义 */
 #define MBEDTLS_BASE64_C                            /** base64 编码 */
 #define MBEDTLS_CERTS_C                             /** 开启 证书模块 */
 #define MBEDTLS_PEM_PARSE_C                         /** pem格式解析 */
 #define MBEDTLS_X509_USE_C                          /** x509 接口 */
 #define MBEDTLS_X509_CRT_PARSE_C                    /** x509 证书解析 */
 
-/** 自定义套件 */
+/** 密钥协商 定义*/
+#define MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED    /** 开启 ECDHE_ECDSA 密钥协商 */
+#define MBEDTLS_KEY_EXCHANGE_ECDHE_RSA_ENABLED      /** 开启 ECDHE_RSA 密钥协商 */
+#define MBEDTLS_KEY_EXCHANGE_PSK_ENABLED            /** 开启 PSK 密钥协商 */
+
+/** 密码套件 定义 */
 /** MBEDTLS_TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256   密钥协商: ECDHE  认证算法: RSA 加密算法: AES_128_GCM 散列算法: SHA256 */
 /** MBEDTLS_TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 密钥协商: ECDHE  认证算法: ECDSA 加密算法: AES_128_GCM 散列算法: SHA256 */
 #if 0
