12d381e51f30e0ee7d249210dc62333034c306eb
xukai <xukai@wsncm.com>
2017-08-05 13:39:58 +0800

    修改X509示例

--- samples/posix/basics/x509/x509_info.c
+++ samples/posix/basics/x509/x509_info.c
@@ -22,27 +22,34 @@
 #include "mbedtls/x509.h"
 #include "mbedtls/ssl.h"
 
-int main( void )
+
+int main(void)
 {
     int ret;
-    char buf[1024];
-    mbedtls_x509_crt srvcert;
+    char buf[4096];
+    const char *cert_buf;
+    int cert_len = 0;
+    mbedtls_x509_crt cert;
 
-    mbedtls_x509_crt_init( &srvcert );
-    ret = mbedtls_x509_crt_parse( &srvcert, (const unsigned char *) mbedtls_test_srv_crt,
-                          mbedtls_test_srv_crt_len );
+    /* */
+    cert_buf = mbedtls_test_ca_crt_ec;
+    cert_len = mbedtls_test_ca_crt_ec_len;
 
-    if( ret != 0 )
-    {
-        mbedtls_printf( " failed\n  !  mbedtls_x509_crt_parse returned %d\n\n", ret );
+    mbedtls_x509_crt_init(&cert);
+    ret = mbedtls_x509_crt_parse(&cert, (const unsigned char *)cert_buf, cert_len);
+    if (ret < 0) {
+        mbedtls_printf("failed: %dX\n", ret);
         goto exit;
     }
 
-    mbedtls_x509_crt_info( buf, sizeof(buf) - 1, "", &srvcert);
-    mbedtls_printf( "%s", buf );
+    mbedtls_x509_crt_info(buf, sizeof(buf) - 1, "", &cert);
+    mbedtls_printf("----------Cert----------\n");
+    mbedtls_printf("Length: %d\n", cert_len);
+    mbedtls_printf("%s", buf);
+
 
 exit:
-    mbedtls_x509_crt_free( &srvcert );
+    mbedtls_x509_crt_free(&cert);
 
     return 0;
 }
