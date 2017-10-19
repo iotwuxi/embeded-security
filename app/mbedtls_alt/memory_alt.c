2fe0a8c133a064382c6831ed39a3f1b8d1cbb777
xukai <xukai19871105@126.com>
2017-10-19 21:28:24 +0800

    修复BUG: platform_calloc编写错误

--- app/mbedtls_alt/memory_alt.c
+++ app/mbedtls_alt/memory_alt.c
@@ -1,6 +1,7 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include <string.h>
 
 /**
  * @brief FreeRTOS平台calloc和free
@@ -9,6 +10,10 @@ void *platform_calloc(size_t n, size_t size)
 {
     void *ptr = NULL; 
     ptr = pvPortMalloc(n * size);
+    if (ptr != NULL) {
+        memset(ptr, 0x00, n * size);
+    }
+        
     return ptr;
 }
 
