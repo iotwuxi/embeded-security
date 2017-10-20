e49c2d4ce1b1c5e63b7d35d30ccb428ee48d171f
xukai <xukai19871105@126.com>
2017-10-20 22:48:31 +0800

    验证selftest中 bignum测试用例通过，basic中原random问题不再出现

--- samples/stm32/basics/basics.c
+++ samples/stm32/basics/basics.c
@@ -1,9 +1,9 @@
 
 #include "basics.h"
 
-#define SAMPLE_RANDOM 				0
+#define SAMPLE_RANDOM 				1
 #define SAMPLE_AES 					0
-#define SAMPLE_CIPHER_MD 			0
+#define SAMPLE_CIPHER_MD 			1
 #define SAMPLE_RSA_ENC 				0
 #define SAMPLE_X509 				0
 #define SAMPLE_MOD_OPERATE			1
@@ -32,7 +32,6 @@ void sample_entry(void)
     sample_mod_operate();
 #endif
     
-    // 可能存在问题
 #if SAMPLE_RANDOM 
 	sample_ctr_random();
 	sample_hmac_random();
