6c383332e7fbcefe8d91b68452b506245a5daccd
xianrenqiu <xianrenqiu90@qq.com>
2017-10-31 21:53:21 +0800

    1. 优化选项会导致malloc地址不对齐，从而进入断言(for循环)。2. 调整freertos堆栈大小为8K，可以通过监控xFreeBytesRemaining变量查看heap使用情况。3. self-test 示例执行正常

--- samples/stm32/self-test/config/FreeRTOSConfig.h
+++ samples/stm32/self-test/config/FreeRTOSConfig.h
@@ -97,12 +97,7 @@
 #define configTICK_RATE_HZ                            ((TickType_t) 1000)
 #define configMAX_PRIORITIES                          (10)
 #define configMINIMAL_STACK_SIZE                      ((uint16_t) 128)
-
-#if defined(__GNUC__)
-#define configTOTAL_HEAP_SIZE                         ((size_t) (25 * 1024))
-#else
-#define configTOTAL_HEAP_SIZE                         ((size_t) (16 * 1024))
-#endif
+#define configTOTAL_HEAP_SIZE                         ((size_t) (8 * 1024))
 
 #define configMAX_TASK_NAME_LEN                       (16)
 #define configUSE_TRACE_FACILITY                      (1)
