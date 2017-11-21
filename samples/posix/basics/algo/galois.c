0fc3f0474a277500f2ea17fcb0f754bcb3a2aa3b
xianrenqiu <xianrenqiu90@qq.com>
2017-11-21 22:15:02 +0800

    增加 galois field 示例(伽罗瓦域乘法运算及指数表生成)

--- samples/posix/basics/algo/galois.c
+++ samples/posix/basics/algo/galois.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 
 #define XTIME(x) ( ( x << 1 ) ^ ( ( x & 0x80 ) ? 0x1B : 0x00 ) )
-
-/*
- * compute pow and log tables over GF(2^8)
- */
-void gen_low_pow_tables(int *pow, int *log, int len)
-{
-    int i, x;
-    for( i = 0, x = 1; i < len; i++ )
-    {
-        *(pow + i) = x;
-        *(log + x) = i;
-        x = ( x ^ XTIME( x ) ) & 0xFF;
-    }    
-}
 
 #define TABLES_HEARD    \
 "   | 0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f \n\
@@ -38,17 +25,64 @@ void dump_tables(char *info, int *tables, int len)
     printf("\n");
 }
 
+uint8_t gmul(uint8_t a, uint8_t b) 
+{
+    uint8_t p = 0;
+    uint8_t counter;
+    uint8_t hi_bit_set;
+
+    for(counter = 0; counter < 8; counter++) 
+    {
+        if((b & 1) == 1) 
+        {
+            p ^= a;
+        }
+
+        hi_bit_set = (a & 0x80);
+        a <<= 1;
+
+        if(hi_bit_set == 0x80) 
+        {
+            a ^= 0x1b;      
+        }
+
+        b >>= 1;
+    }
+
+    return p;
+}
+
+/*
+ * compute pow and log tables over GF(2^8)
+ */
+void gen_tables(int *pow, int *log, int len)
+{
+    int i, x;
+    for( i = 0, x = 1; i < len; i++ )
+    {
+        *(pow + i) = x;
+        *(log + x) = i;
+        x = ( x ^ XTIME( x ) ) & 0xFF;
+    }    
+}
+
 /** 
 *   http://www.samiam.org/logtables.txt  Generator: 0x03
+*   http://www.samiam.org/galois.html  Exponents and logarithms
 */
 int main(int argc, char const *argv[])
 {
+    uint8_t a = 0xe5;
+    uint8_t b = 0xe5;
+    printf("gmul(%02x, %02x) = %02x\n\n", a, b, gmul(a, b));
+
     int pow[256];
     int log[256];
+
     memset(pow, 0, sizeof(pow));
     memset(log, 0, sizeof(log));
 
-    gen_low_pow_tables(pow, log, 256);
+    gen_tables(pow, log, 256);
 
     dump_tables("Log table:", log, 256);
     dump_tables("Pow table:", pow, 256);
