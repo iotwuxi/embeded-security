a4fca29d48b52cde5557294f5d5944cabbe86fae
xianrenqiu <xianrenqiu90@qq.com>
2017-11-22 21:38:18 +0800

    增加 aes sbox 表生成代码

--- samples/posix/basics/algo/galois.c
+++ samples/posix/basics/algo/galois.c
@@ -3,7 +3,8 @@
 #include <string.h>
 
 #define SET_G_ENABLE 1
-#define XTIME(x) ( ( x << 1 ) ^ ( ( x & 0x80 ) ? 0x1B : 0x00 ) )
+#define XTIME(x)        ( ( x << 1 ) ^ ( ( x & 0x80 ) ? 0x1B : 0x00 ) )
+#define ROTL8(x, shift)  ((uint8_t) ((x) << (shift)) | ((x) >> (8 - (shift))))
 
 #define TABLES_HEARD    \
 "   | 0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f \n\
@@ -89,9 +90,35 @@ void gen_tables(int *pow, int *log, int *inv, int len)
 }
 #endif
 
+void gen_aes_sbox(int sbox[256]) 
+{
+    uint8_t p = 1, q = 1;
+    
+    /* loop invariant: p * q == 1 in the Galois field */
+    do {
+        /* multiply p by 2 */
+        p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
+
+        /* divide q by 2 */
+        q ^= q << 1;
+        q ^= q << 2;
+        q ^= q << 4;
+        q ^= q & 0x80 ? 0x09 : 0;
+
+        /* compute the affine transformation */
+        uint8_t xformed = q ^ ROTL8(q, 1) ^ ROTL8(q, 2) ^ ROTL8(q, 3) ^ ROTL8(q, 4);
+
+        sbox[p] = xformed ^ 0x63;
+    } while (p != 1);
+
+    /* 0 is a special case since it has no inverse */
+    sbox[0] = 0x63;
+}
+
 /** 
 *   http://www.samiam.org/logtables.txt  Generator: 0x03
 *   http://www.samiam.org/galois.html  Exponents and logarithms
+*   https://en.wikipedia.org/wiki/Rijndael_S-box
 */
 int main(int argc, char const *argv[])
 {
@@ -102,6 +129,7 @@ int main(int argc, char const *argv[])
     int pow[256];
     int log[256];
     int inv[256];
+    int sbox[256];
 
     memset(pow, 0, sizeof(pow));
     memset(log, 0, sizeof(log));
@@ -113,9 +141,12 @@ int main(int argc, char const *argv[])
     gen_tables(pow, log, inv, 256);
 #endif
 
+    gen_aes_sbox(sbox);
+
     dump_tables("Log table:", log, 256);
     dump_tables("Pow table:", pow, 256);
     dump_tables("Inv table:", inv, 256);
+    dump_tables("Sbox table:", sbox, 256);
 
     return 0;
 }
