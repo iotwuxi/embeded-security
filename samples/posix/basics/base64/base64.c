8445916040072493571a4dc8c8d90ded1a4e5778
xukai <xukai19871105@126.com>
2018-02-21 12:31:29 +0800

    修改base64示例

--- samples/posix/basics/base64/base64.c
+++ samples/posix/basics/base64/base64.c
@@ -3,43 +3,38 @@
 #include <stdint.h>
 #include "mbedtls/base64.h"
 
-/** 测试样本来自 https://en.wikipedia.org/wiki/Base64#Examples */
+#define mbedtls_printf     printf
 
-static char *msg = 
-"Man is distinguished, not only by his reason, but by this singular passion from \
-other animals, which is a lust of the mind, that by a perseverance of delight \
-in the continued and indefatigable generation of knowledge, exceeds the short \
-vehemence of any carnal pleasure.";
+// 样本来自于 https://tools.ietf.org/html/rfc3548
+// The Base16, Base32, and Base64 Data Encodings
+static uint8_t msg[] = {0x14, 0xfb, 0x9c, 0x03, 0xd9, 0x7e};
 
-static char *base64 = 
-"TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz\
-IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg\
-dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu\
-dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo\
-ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
-
-int main( void )
+int main(void)
 {
     size_t len;
-    char rst[512];
+    uint8_t rst[512];
 
-    printf("\nBase64 encode msg[%ld]: \n%s\n\n", strlen(msg), msg);
-
-    if( mbedtls_base64_encode(rst, sizeof(rst), &len, msg, strlen(msg)) != 0 )
-    {
-        printf("base64 encode failed\n");
-        return -1;
-    }
-    printf("Base64 encode result[%ld]: \n%s\n\n", len, rst);
-
-    if( mbedtls_base64_decode(rst, sizeof(rst), &len, rst, len) != 0 )
-    {
-        printf("base64 decode failed\n");
-        return -1;
+    len = sizeof(msg)/ sizeof(msg[0]);
+    mbedtls_printf("\nBase64 Encode Message[%ld]: ", len);
+    for (uint8_t i = 0; i < len; i++) {
+        mbedtls_printf("%02x ", msg[i]);
     }
 
-    rst[len] = 0;
-    printf("Base64 decode result[%ld]: \n%s\n\n", len, rst);
+    if (mbedtls_base64_encode(rst, sizeof(rst), &len, msg, len) != 0) {
+        mbedtls_printf("\nBase64 Encode Failed\n");
+        return -1;
+    }
+    mbedtls_printf("\nBase64 Encode Result[%ld]: %s", len, rst);
+
+    if (mbedtls_base64_decode(rst, sizeof(rst), &len, rst, len) != 0 ) {
+        mbedtls_printf("\nBase64 Decode failed\n");
+        return -1;
+    }
+
+    mbedtls_printf("\nBase64 Decode Result[%ld]: ", len);
+    for (uint8_t i = 0; i < len; i++) {
+        mbedtls_printf("%02x ", rst[i]);
+    }
 
     return 0;
 }
