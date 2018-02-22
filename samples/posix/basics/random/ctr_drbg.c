7bfa0e647fd7e1386eccea72cf01ed96d5a454d9
xukai <xukai19871105@126.com>
2018-02-22 18:01:37 +0800

    代码整理

--- samples/posix/basics/random/ctr_drbg.c
+++ samples/posix/basics/random/ctr_drbg.c
@@ -4,33 +4,34 @@
 
 static unsigned char random[64];
 
-int main( void )
+#define mbedtls_printf printf
+
+int main(void)
 {
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
 
-    mbedtls_entropy_init( &entropy );
-    mbedtls_ctr_drbg_init( &ctr_drbg );
+    mbedtls_entropy_init(&entropy);
+    mbedtls_ctr_drbg_init(&ctr_drbg);
 
-    int ret = mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char *) "RANDOM_GEN", 10 );
-    if( ret != 0 )
-    {
-        printf( "failed in mbedtls_ctr_drbg_seed: %d\n", ret );
+    int ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, 
+        (const unsigned char *) "RANDOM_GEN", 10);
+    if (ret != 0) {
+        mbedtls_printf("failed in mbedtls_ctr_drbg_seed: %d\n", ret);
         goto exit;
     }
-    mbedtls_ctr_drbg_set_prediction_resistance( &ctr_drbg, MBEDTLS_CTR_DRBG_PR_OFF );
+    mbedtls_ctr_drbg_set_prediction_resistance(&ctr_drbg, MBEDTLS_CTR_DRBG_PR_OFF);
 
-    ret = mbedtls_ctr_drbg_random( &ctr_drbg, random, sizeof(random) );
-    if( ret != 0 )
-    {
-        printf( "failed in mbedtls_ctr_drbg_random: %d\n", ret );
+    ret = mbedtls_ctr_drbg_random(&ctr_drbg, random, sizeof(random));
+    if (ret != 0) {
+        mbedtls_printf("failed in mbedtls_ctr_drbg_random: %d\n", ret);
         goto exit;
     }
 
-    printf("Generating %ld byte random data : ", sizeof(random));
-    for(int i = 0; i < sizeof(random); i++)
-    {
-        printf("%s%02X%s", i % 16 == 0 ? "\n":" ", random[i], i == sizeof(random) - 1 ? "\n":"");
+    mbedtls_printf("Generating %ld byte random data : ", sizeof(random));
+    for (int i = 0; i < sizeof(random); i++) {
+        mbedtls_printf("%s%02X%s", i % 16 == 0 ? "\n":" ", 
+            random[i], i == sizeof(random) - 1 ? "\n":"");
     }
 
 exit:
